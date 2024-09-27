4cb33dae31d6fd1cad0c6cb1b1a7b7d5c39467d4
juliemeta <juliemonefeldt_404@hotmail.com>
2024-09-27 13:37:26 +0200

    Added basic search engine - no API yet

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-alert("Hello!");
+function handleCitySearch(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#city-search-form-input");
+  let currentCityElement = document.querySelector("#current-city");
+  currentCityElement.innerHTML = searchInput.value;
+}
+let SearchFormElement = document.querySelector("#city-search-form");
+SearchFormElement.addEventListener("submit", handleCitySearch);

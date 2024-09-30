20d7f35efd23671e391b4de31237c3c630523e3d
juliemeta <juliemonefeldt_404@hotmail.com>
2024-09-30 13:33:33 +0200

    New functions that update searched city name and its current temperature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
+function updateCurrentTemperature(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city");
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "c2t7ea4432f52e0o6d402fd54c5bc269";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateCurrentTemperature);
+}
+
 function handleCitySearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#city-search-form-input");
   let currentCityElement = document.querySelector("#current-city");
   currentCityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
+
 let SearchFormElement = document.querySelector("#city-search-form");
 SearchFormElement.addEventListener("submit", handleCitySearch);
